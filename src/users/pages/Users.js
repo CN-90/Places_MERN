5fae700bb72b645e28cb8c76f370959cee086330
Christian <chr.revel@gmail.com>
2019-12-13 10:03:29 -0700

    Created Navigation, card, avatar, user item and user list components

--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import UsersList from './../components/users-list/UsersList';
 
 const Users = () => {
-  return <h2>Users works</h2>;
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Harry Potter',
+      image:
+        'https://images.pexels.com/photos/3233372/pexels-photo-3233372.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      places: 3
+    }
+  ];
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
